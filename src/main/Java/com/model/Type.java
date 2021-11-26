ce7cb84d04df848124d7cd6a2f15bb000e73f814
a <1179938183@qq.com>
2021-11-27 05:01:38 +0800

    定义资源类别实体类

---
+++ src/main/Java/com/model/Type.java
@@ -0,0 +1,40 @@
+package com.model;
+
+public class Type {
+    // 标识
+    private Integer id;
+    // 资源类别
+    private String type;
+
+    public Type() {
+    }
+
+    public Type(Integer id, String type) {
+        this.id = id;
+        this.type = type;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Type{" +
+                "id=" + id +
+                ", type='" + type + '\'' +
+                '}';
+    }
+}
