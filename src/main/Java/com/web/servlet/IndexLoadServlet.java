a9468d0f5276abf123f1b874d37c368b63c7e546
a <1179938183@qq.com>
2021-11-30 03:19:53 +0800

    主页面加载

---
+++ src/main/Java/com/web/servlet/IndexLoadServlet.java
@@ -0,0 +1,84 @@
+package com.web.servlet;
+
+import com.dao.impl.ResourceDaoImpl;
+import com.model.Page;
+import com.model.Resource;
+import com.model.User;
+import com.utils.JsonUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * 主页面加载请求
+ */
+public class IndexLoadServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        // 判断请求类型
+        String type = (String)request.getAttribute("type");
+        if("indexload".equals(type)){
+            // 主页面加载
+            indexLoad(request,response);
+        }else{
+            // 退出登录
+            logout(request);
+        }
+    }
+
+    /**
+     * 主页面加载（登录状态检查、资源加载）
+     * @param request
+     * @param response
+     */
+    private void indexLoad(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // 检查用户是否已登录
+        HttpSession session = request.getSession(false);
+        // 记录登录状态
+        boolean isLogin = false;
+        String username = "";
+        if(session != null && session.getAttribute("user") != null){
+            // 用户已登录
+            isLogin = true;
+            // 用户名
+            User user = (User)session.getAttribute("user");
+            username = user.getName();
+        }
+
+        // 查询数据库，加载第一页的资源
+        // 资源总条数
+        Long totalsize = new ResourceDaoImpl().getNum();
+        // 资源数据
+        List<Resource> list = new ResourceDaoImpl().getAllResource(1,10);
+
+        // 将资源包装为对象
+        Page<Resource> page = new Page(1, totalsize, list);
+        // 将对象转换为
+        String json = JsonUtil.toJson(page);
+
+        // 查询结果
+        String result = "[{ isLogin : " + isLogin + ",username : \"" + username + "\"}," + json + "]";
+
+        response.getWriter().print(result);
+    }
+
+    /**
+     * 退出登录
+     * @param request
+     */
+    private void logout(HttpServletRequest request){
+        // 判断用户是否已登录
+        HttpSession session = request.getSession(false);
+        // 如果用户已登录，注销session
+        if(session != null && session.getAttribute("user") != null){
+            session.invalidate();
+        }
+    }
+}
